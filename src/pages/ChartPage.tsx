76d35390756671effd40d50e1e03f37f1b430dcd
lyeejj <lyshine012@gmail.com>
2023-09-13 19:18:56 +0900

    feat: ChartPage 제목 추가

--- src/pages/ChartPage.tsx
+++ src/pages/ChartPage.tsx
@@ -1,10 +1,19 @@
+import styled from '@emotion/styled';
 import Chart from '../components/Chart';
 
 function ChartPage() {
 	return (
 		<>
+			<ChartTitle>Time Series Chart</ChartTitle>
 			<Chart />
 		</>
 	);
 }
 export default ChartPage;
+
+const ChartTitle = styled.h1`
+	font-size: 1.5rem;
+	text-align: center;
+	margin: 45px;
+	color: #505050;
+`;
